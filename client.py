dbd1927454296b5ead99f36c43fd4847d751f781
Fitsum Alemu <fitsumcs@gmail.com>
2020-12-11 21:28:53 +0300

    Adding clinet

---
+++ client.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('192.168.10.120', 8080)) #IP is the server IP
+ 
+for args in sys.argv:
+    if args == "":
+        args = "no args"
+    else:
+        byt=args.encode()
+        #s.send(args)
+        s.send(byt)
+ 
+print('goodbye!')

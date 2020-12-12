0d8267399f48c3d4f169c54b137e264aaedcd317
Fitsum Alemu <fitsumcs@gmail.com>
2020-12-12 14:05:45 +0300

    few changes

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import sys
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.10.120', 8080))                   #IP is the server IP
+s.connect(('192.168.10.101', 8080))                   #IP is the server IP
 
 #replace your text here
 text_To_Send=  "Hello There !!!"
